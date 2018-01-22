eb39c6345af44783922448aed36ac083f62465e8
FjeldMats <FjeldMats@Gmail.com>
2018-01-22 18:19:55 +0100

    .

--- Java/problem47.java
+++ Java/problem47.java
@@ -1,32 +1,30 @@
 import java.util.ArrayList;
 
-public class euler47 {
+public class problem47 {
 
 	public static void main(String[] args) {
 	
 		int i = 2;
 		int previ = 1;
-		int inarow = 0;
+		int inarow = 0; 
 		
-		while(inarow <= 2) {
-			if(countPrimeFactors(findFactors(i)) >= 2) {
+		while(inarow < 3) {
+			if(countPrimeFactors(findFactors(i)) == 4) {
 				System.out.println(i+ " " + findFactors(i));
 				if(previ + 1 == i) {
 					inarow++;
-				}
-				else {
+				}else {
 					inarow = 0;
 				}
+				previ = i;
 			}
 			
-			
-			previ = i;
 			i++;
 		}
 	}
 	
 	public static boolean isPrime(double n) {
-		for(double i = 2; i < (int) Math.sqrt(n) + 5; i++) {
+		for(double i = 2; i < n; i++) { 
 			if(n%i == 0) {
 				return false;
 			}
@@ -37,7 +35,7 @@ public class euler47 {
 	public static ArrayList<Integer> findFactors(long n){
 		
 		ArrayList<Integer> factors = new ArrayList<Integer>();
-		for(int i=2; i< (int) Math.sqrt(n) + 5; i++ ) {
+		for(int i=2; i< n; i++ ) {
 			if(n%i == 0) {
 				factors.add(i);
 			}
