35ab529f044a542c14adb7ed252490a8a98dac90
FjeldMats <FjeldMats@Gmail.com>
2019-01-20 14:18:03 +0100

    Create p17.c

---
+++ C/p17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    // 1 - 19
+    int lettercount1[19] = {3,3,5,4,4,3,5,5,4,3,6,6,8,8,7,7,9,8,8,6};
+
+    //20,30,40 ... 100
+    int lettercount2[7] = {6,6,5,5,7,6,6};
+    int hundred = 7;
+    int and = 3;
+
+    int sum = 0;
+
+    // 1-19
+    for(int i=0; i<19;i++){
+        sum += lettercount1[i];
+    }
+
+    //20 - 99
+    for (int i=20; i<99;i++){
+        
+    }
+}
